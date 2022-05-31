b76d5297822d117f6bba7acea078ec0cbf1070b2
Benjamin Barbier <benjamin@tech-homies.io>
2022-05-31 23:35:40 +0200

    :sparkles: implementation with custom operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,32 @@
 import {Component} from '@angular/core';
-import {
-  debounce,
-  debounceTime,
-  distinctUntilChanged, distinctUntilKeyChanged,
-  map,
-  mapTo,
-  merge,
-  mergeMap,
-  of,
-  pairwise,
-  Subject,
-  timer
-} from 'rxjs';
+import {debounceTime, distinctUntilChanged, merge, mergeMap, Observable, of, pairwise, Subject} from 'rxjs';
+
+const DEBOUNCE_TIME_IN_MS = 1_000;
 
 interface Item {
   id: string,
   value: number,
+}
+
+function customDebounce(time: number) {
+  const debounceQueueSubject = new Subject<Item>();
+  const debounceQueue = debounceQueueSubject.pipe(debounceTime(time));
+
+  return function (source: Observable<Item>): Observable<Item> {
+    return source.pipe(
+      pairwise(),
+      mergeMap(([oldItem, newItem]: [Item, Item]) => {
+        if (oldItem.id === newItem.id) {
+          debounceQueueSubject.next(newItem);
+          return debounceQueue;
+        } else {
+          debounceQueueSubject.next(newItem);
+          return of(oldItem);
+        }
+      }),
+      distinctUntilChanged((oldItem, newItem) => JSON.stringify(oldItem) === JSON.stringify(newItem)),
+    )
+  };
 }
 
 @Component({
@@ -29,51 +40,23 @@ export class AppComponent {
   public b: number = 0;
   public c: number = 0;
 
-  public readonly subjectA: Subject<'A'> = new Subject<'A'>();
-  public readonly subjectB: Subject<'B'> = new Subject<'B'>();
-  public readonly subjectC: Subject<'C'> = new Subject<'C'>();
+  public readonly subjectA = new Subject<Item>();
+  public readonly subjectB = new Subject<Item>();
+  public readonly subjectC = new Subject<Item>();
+
+  public incrementAndEmitA = () => this.subjectA.next({id: 'A', value: ++this.a});
+  public incrementAndEmitB = () => this.subjectA.next({id: 'B', value: ++this.b});
+  public incrementAndEmitC = () => this.subjectA.next({id: 'C', value: ++this.c});
+
   public output: string = '';
 
   constructor() {
-
-    const debounceQueueSubject = new Subject<Item>();
-    const debounceQueue = debounceQueueSubject.pipe(debounceTime(1000));
-
-    // Test 3
     merge(
-      this.subjectA.pipe(map(() => ({ id: 'A', value: this.a }))),
-      this.subjectB.pipe(map(() => ({ id: 'B', value: this.b }))),
-      this.subjectC.pipe(map(() => ({ id: 'C', value: this.c }))),
+      this.subjectA,
+      this.subjectB,
+      this.subjectC,
     ).pipe(
-      pairwise(),
-      mergeMap(([oldLetter, newLetter]: [Item, Item]) => {
-        if (oldLetter.id === newLetter.id) {
-          debounceQueueSubject.next(newLetter);
-          return debounceQueue;
-        } else {
-          debounceQueueSubject.next(newLetter);
-          return of(oldLetter);
-        }
-      }),
-      distinctUntilChanged((oldItem, newItem) => JSON.stringify(oldItem) === JSON.stringify(newItem))
+      customDebounce(DEBOUNCE_TIME_IN_MS),
     ).subscribe(letter => this.output += JSON.stringify(letter) + '\n')
-
-    // Test 1 ❌
-    // merge(this.subjectA, this.subjectB, this.subjectC).pipe(
-    //   debounceTime(1000),
-    // ).subscribe(letter => this.output += letter + '\n')
-
-    // Test 2 ❌
-    // merge(this.subjectA, this.subjectB, this.subjectC).pipe(
-    //   pairwise(),
-    //   debounce(([oldLetter, newLetter]) => {
-    //     if (oldLetter !== newLetter) {
-    //       return timer(0);
-    //     }
-    //     return timer(1000);
-    //   }),
-    //   map(([oldLetter, newLetter]) => newLetter),
-    // ).subscribe(letter => this.output += letter + '\n')
   }
-
 }
